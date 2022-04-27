1ba0d8161df982f59cfb27c7c6c17eed1ece494f
Tobias Andersen <toban@dfds.com>
2022-04-28 00:51:51 +0200

    - Updating namespace and paths

---
+++ src/DigitalTwins.Device.Simulator/start.sh
@@ -0,0 +1,3 @@
+az iot hub device-identity create --device-id $AZ_IOT_DEVICE_ID --hub-name $AZ_IOT_HUB_NAME 2>nul
+
+az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB_NAME
--- src/IoTDeviceSimulator/start.sh
+++
@@ -1,3 +0,0 @@
-az iot hub device-identity create --device-id $AZ_IOT_DEVICE_ID --hub-name $AZ_IOT_HUB 2>nul
-
-az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB
