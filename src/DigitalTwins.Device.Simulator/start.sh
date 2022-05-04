11a6b0ff3de90308c3ddc72a87f433672ef71f9a
Tobias Andersen <toban@dfds.com>
2022-05-04 20:06:28 +0200

    - Added response code and payload to simulation command call to AZ CLI in Simulator

--- src/DigitalTwins.Device.Simulator/start.sh
+++ src/DigitalTwins.Device.Simulator/start.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
 az iot hub device-identity create --device-id $AZ_IOT_DEVICE_ID --hub-name $AZ_IOT_HUB_NAME 2>nul
 
-az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB_NAME
+methodResponsePayload=$(cat <<EOF
+{
+  "result": "Direct method successful."
+}
+EOF
+)
+
+az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB_NAME --method-response-code 201 --method-response-payload $methodResponsePayload
