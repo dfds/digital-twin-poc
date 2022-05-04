7ff99e7d0fe9231d906668728d687157c0a1639f
Tobias Andersen <toban@dfds.com>
2022-05-04 20:17:45 +0200

    .

--- src/DigitalTwins.Device.Simulator/start.sh
+++ src/DigitalTwins.Device.Simulator/start.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
 az iot hub device-identity create --device-id $AZ_IOT_DEVICE_ID --hub-name $AZ_IOT_HUB_NAME 2>nul
 
-methodResponsePayload=$(cat <<EOF
-{
-  "result": "Direct method successful."
-}
-EOF
-)
-
-az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB_NAME --method-response-code 201 --method-response-payload $methodResponsePayload
+az iot device simulate -d $AZ_IOT_DEVICE_ID -n $AZ_IOT_HUB_NAME --method-response-code 201 --method-response-payload '{"result":"Direct method successful"}'
